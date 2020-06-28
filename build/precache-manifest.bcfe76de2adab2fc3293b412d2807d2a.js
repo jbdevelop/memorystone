27686cb91680f05a8a503e78210236c4e6e9a689
jbdevelop <jbernardorj@gmail.com>
2020-06-28 14:24:01 -0300

    Build

---
+++ build/precache-manifest.bcfe76de2adab2fc3293b412d2807d2a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6609b33d2fe2f253e8e407faaaded67b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdcfe90b0bbbd9dea9a5",
+    "url": "/static/css/main.145b753f.chunk.css"
+  },
+  {
+    "revision": "e77bd9d268f212699f5c",
+    "url": "/static/js/2.5028e1ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5028e1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdcfe90b0bbbd9dea9a5",
+    "url": "/static/js/main.fd1577ab.chunk.js"
+  },
+  {
+    "revision": "7ca845aa31278db932b1",
+    "url": "/static/js/runtime-main.40d72249.js"
+  },
+  {
+    "revision": "c0814bd555b1aac8dfaa5f6030c5e526",
+    "url": "/static/media/loading.c0814bd5.gif"
+  },
+  {
+    "revision": "09742d6dd95f7b5076f515f1a528c502",
+    "url": "/static/media/logo_hearthstone.09742d6d.png"
+  },
+  {
+    "revision": "75fa7662e67cb21aa4e258f2607d9cad",
+    "url": "/static/media/poster1.75fa7662.jpg"
+  },
+  {
+    "revision": "dd39ba9d9ad0cc0c3defb7fdccc26262",
+    "url": "/static/media/poster2.dd39ba9d.jpg"
+  },
+  {
+    "revision": "03de35aa078878e15807de9154c0d070",
+    "url": "/static/media/poster3.03de35aa.jpg"
+  },
+  {
+    "revision": "eba7ec5bff1c1c858003624a58d66bb2",
+    "url": "/static/media/poster4.eba7ec5b.jpg"
+  },
+  {
+    "revision": "09cae375e2d2a61496b7ca5a03e4c52a",
+    "url": "/static/media/poster5.09cae375.jpg"
+  },
+  {
+    "revision": "6ff0c46abb043ac7b811c0c3abe513ad",
+    "url": "/static/media/poster6.6ff0c46a.jpg"
+  },
+  {
+    "revision": "1b2c1f8a0a7c3807eb620c7d1d556a91",
+    "url": "/static/media/poster7.1b2c1f8a.jpg"
+  }
+]);
